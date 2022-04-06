b669030025cd537fb603cc4bc7d1c640973a3c3e
Jamie Lin <cuprid@gmail.com>
2022-04-06 12:39:48 +0700

    first init

--- config.py
+++ config.py
@@ -5,18 +5,18 @@
 # ----------------------------------------------- #
 
 # TradingView Example Alert Message:
-# {
-# "key":"9T2q394M92", "telegram":"-1001298977502", "discord":"789842349670960670/BFeBBrCt-w2Z9RJ2wlH6TWUjM5bJuC29aJaJ5OQv9sE6zCKY_AlOxxFwRURkgEl852s3", "msg":"Long #{{ticker}} at `{{close}}`"
-# }
+{
+    "key":"gundulPacul777", "telegram":"-1001672905672", "msg":"Long #{{ticker}} at `{{close}}`"
+}
 
 sec_key = (
-    ""  # Can be anything. Has to match with "key" in your TradingView alert message
+    "gundulPacul777"  # Can be anything. Has to match with "key" in your TradingView alert message
 )
 
 # Telegram Settings
 send_telegram_alerts = False
-tg_token = ""  # Bot token. Get it from @Botfather
-channel = 0  # Channel ID (ex. -1001487568087)
+tg_token = "5056572283:AAF4xqvg_FIqqEw-U3feNW0rN4tYMRBC9gk"  # Bot token. Get it from @Botfather
+channel = -1001672905672  # Channel ID (ex. -1001487568087)
 
 # Discord Settings
 send_discord_alerts = False
